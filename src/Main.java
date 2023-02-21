043a5050b3854664d4265d5b09489fccb45c2f62
SirJastin <sirjastin@gmail.com>
2023-02-21 17:20:32 +0100

    Commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hi my first repository is update on bransh main");
+    }
+}
